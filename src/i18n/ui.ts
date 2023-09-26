fbeb313296b1db8314eaf7eec00ef8efaf9a58bb
Jair Aya <jair.aya@uniminuto.edu.co>
2023-09-25 19:22:57 -0500

    first commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,31 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+};
+
+export const languagesFlags = {
+    en: "emojione:flag-for-united-kingdom",
+    es: "emojione:flag-for-spain",
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        'sidebar.home': 'Home',
+        'sidebar.projects': 'Projects',
+        'sidebar.contact': 'Contact',
+    },
+    es: {
+        'sidebar.home': 'Inicio',
+        'sidebar.projects': 'Proyectos',
+        'sidebar.contact': 'Contacto',
+    },
+} as const;
+
+export const routes = {
+    es: {
+        projects: 'proyectos',
+    }
+} as const;
