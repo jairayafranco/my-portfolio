382aea78e9637653e6a4e8add7a038a3e4803301
Jair Aya <jair.aya@uniminuto.edu.co>
2023-10-09 16:54:14 -0500

    added favicon and projects view finished

---
+++ src/data/projects.ts
@@ -0,0 +1,62 @@
+export const projects = [
+    {
+        name: "Notes App",
+        description: "A simple notes app built with React and TypeScript.",
+        image: "notes-app.png",
+        github: "https://github.com/jairayafranco/Notes-App-Front"
+    },
+    {
+        name: "Photo Album",
+        description: "A photo album app where allows you to create and browse photo galleries quickly and easily built with Next.js, Tailwind CSS, TypeScript and Cloudinary.",
+        image: "photo-album.png",
+        github: "https://github.com/jairayafranco/Photo-Album"
+    },
+    {
+        name: "Cohere AI Chat",
+        description: "A chat application based on artificial intelligence that utilizes Next.js and Cohere to provide an intelligent and personalized conversation experience.",
+        image: "cohere-ai-chat.png",
+        github: "https://github.com/jairayafranco/Cohere-AIchat"
+    },
+    {
+        name: "Games Store",
+        description: "A simple games store built with Astro and using view transitions to create a smooth user experience.",
+        image: "games-store.png",
+        github: "https://github.com/jairayafranco/GamesStore-Viewtransition"
+    },
+    {
+        name: "BooksHub",
+        description: "A simple books store where users can find a wide variety of books and keep track of their personal library. Built with React, TypeScript, Zustand and Tailwind CSS.",
+        image: "bookshub.png",
+        github: "https://github.com/jairayafranco/BookHub"
+    },
+    {
+        name: "MedicApp",
+        description: "This is an app made to present as a final degree project that allows doctors to find and book appointments of their patients. Built with React, TypeScript, Zustand and MUI.",
+        image: "medicapp.png",
+        github: "https://github.com/jairayafranco/medic-app-next"
+    },
+    {
+        name: "Chat App",
+        description: "A simple chat app built with React, Socket.IO and Tailwind CSS.",
+        image: "chat-app.png",
+        github: "https://github.com/jairayafranco/chat-client"
+    },
+    {
+        name: "Anime Tracker",
+        description: "Anime Tracker is a React application that allows users to search for anime titles and save them to the local storage for future reference. Built with React, Zustand and Mantine.",
+        image: "anime-tracker.png",
+        github: "https://github.com/jairayafranco/anime-tracker"
+    },
+    {
+        name: "Debts App",
+        description: "A debt management application built with React and Firebase for tracking and managing debts.",
+        image: "debts-app.png",
+        github: "https://github.com/jairayafranco/Debts-App"
+    },
+    {
+        name: "Japanese Quiz",
+        description: "A simple Japanese quiz app built with React and Tailwind CSS.",
+        image: "japanese-quiz.png",
+        github: "https://github.com/jairayafranco/japanese-quiz"
+    }
+]
