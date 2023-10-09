c9b07dc643ad3fb3a58d18dcf0fd25675f5163d0
Jair Aya <jair.aya@uniminuto.edu.co>
2023-10-09 17:04:22 -0500

    added projects and index in spanish view

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,61 +1,91 @@
 export const projects = [
     {
         name: "Notes App",
-        description: "A simple notes app built with React and TypeScript.",
+        description: {
+            en: "A simple notes app built with React and TypeScript.",
+            es: "Una aplicación de notas simple creada con React y TypeScript."
+        },
         image: "notes-app.png",
         github: "https://github.com/jairayafranco/Notes-App-Front"
     },
     {
         name: "Photo Album",
-        description: "A photo album app where allows you to create and browse photo galleries quickly and easily built with Next.js, Tailwind CSS, TypeScript and Cloudinary.",
+        description: {
+            en: "A photo album app where allows you to create and browse photo galleries quickly and easily built with Next.js, Tailwind CSS, TypeScript and Cloudinary.",
+            es: "Una aplicación de álbum de fotos donde puedes crear y navegar por galerías de fotos de forma rápida y sencilla creada con Next.js, Tailwind CSS, TypeScript y Cloudinary."
+        },
         image: "photo-album.png",
         github: "https://github.com/jairayafranco/Photo-Album"
     },
     {
         name: "Cohere AI Chat",
-        description: "A chat application based on artificial intelligence that utilizes Next.js and Cohere to provide an intelligent and personalized conversation experience.",
+        description: {
+            en: "A chat application based on artificial intelligence that utilizes Next.js and Cohere to provide an intelligent and personalized conversation experience.",
+            es: "Una aplicación de chat basada en inteligencia artificial que utiliza Next.js y Cohere para proporcionar una experiencia de conversación inteligente y personalizada."
+        },
         image: "cohere-ai-chat.png",
         github: "https://github.com/jairayafranco/Cohere-AIchat"
     },
     {
         name: "Games Store",
-        description: "A simple games store built with Astro and using view transitions to create a smooth user experience.",
+        description: {
+            en: "A simple games store built with Astro and using view transitions to create a smooth user experience.",
+            es: "Una tienda de juegos simple creada con Astro y usando view transitions para crear una experiencia de usuario fluida."
+        },
         image: "games-store.png",
         github: "https://github.com/jairayafranco/GamesStore-Viewtransition"
     },
     {
         name: "BooksHub",
-        description: "A simple books store where users can find a wide variety of books and keep track of their personal library. Built with React, TypeScript, Zustand and Tailwind CSS.",
+        description: {
+            en: "A simple books store where users can find a wide variety of books and keep track of their personal library. Built with React, TypeScript, Zustand and Tailwind CSS.",
+            es: "Una tienda de libros simple donde los usuarios pueden encontrar una gran variedad de libros y realizar un seguimiento de su biblioteca personal. Creado con React, TypeScript, Zustand y Tailwind CSS."
+        },
         image: "bookshub.png",
         github: "https://github.com/jairayafranco/BookHub"
     },
     {
         name: "MedicApp",
-        description: "This is an app made to present as a final degree project that allows doctors to find and book appointments of their patients. Built with React, TypeScript, Zustand and MUI.",
+        description: {
+            en: "This is an app made to present as a final degree project that allows doctors to find and book appointments of their patients. Built with React, TypeScript, Zustand and MUI.",
+            es: "Esta es una aplicación hecha para presentar como proyecto final de grado que permite a los médicos encontrar y reservar citas de sus pacientes. Creado con React, TypeScript, Zustand y MUI."
+        },
         image: "medicapp.png",
         github: "https://github.com/jairayafranco/medic-app-next"
     },
     {
         name: "Chat App",
-        description: "A simple chat app built with React, Socket.IO and Tailwind CSS.",
+        description: {
+            en: "A simple chat app built with React, Socket.IO and Tailwind CSS.",
+            es: "Una aplicación de chat simple creada con React, Socket.IO y Tailwind CSS."
+        },
         image: "chat-app.png",
         github: "https://github.com/jairayafranco/chat-client"
     },
     {
         name: "Anime Tracker",
-        description: "Anime Tracker is a React application that allows users to search for anime titles and save them to the local storage for future reference. Built with React, Zustand and Mantine.",
+        description: {
+            en: "Anime Tracker is a React application that allows users to search for anime titles and save them to the local storage for future reference. Built with React, Zustand and Mantine.",
+            es: "Anime Tracker es una aplicación de React que permite a los usuarios buscar títulos de anime y guardarlos en el almacenamiento local para futuras referencias. Creado con React, Zustand y Mantine."
+        },
         image: "anime-tracker.png",
         github: "https://github.com/jairayafranco/anime-tracker"
     },
     {
         name: "Debts App",
-        description: "A debt management application built with React and Firebase for tracking and managing debts.",
+        description: {
+            en: "A debt management application built with React and Firebase for tracking and managing debts.",
+            es: "Una aplicación de gestión de deudas creada con React y Firebase para realizar un seguimiento y administrar las deudas."
+        },
         image: "debts-app.png",
         github: "https://github.com/jairayafranco/Debts-App"
     },
     {
         name: "Japanese Quiz",
-        description: "A simple Japanese quiz app built with React and Tailwind CSS.",
+        description: {
+            en: "A simple Japanese quiz app built with React and Tailwind CSS.",
+            es: "Una aplicación de preguntas y respuestas simple creada con React y Tailwind CSS."
+        },
         image: "japanese-quiz.png",
         github: "https://github.com/jairayafranco/japanese-quiz"
     }
